5f7df12b9fd0fc53e62ae23b8d79c53d408f90c1
ethanmaxey <ethanm113@gmail.com>
2024-09-12 22:03:05 -0500

    Seperated out view and viewModel kinda. UI updates

---
+++ Track/View Model/ContentViewModel.swift
@@ -0,0 +1,42 @@
+//
+//  ContentViewModel.swift
+//  Track
+//
+//  Created by Ethan Maxey on 9/12/24.
+//
+
+import SwiftUI
+
+extension ContentView {
+    private func addjob(name: String) {
+        withAnimation {
+            let newJob = JobListing(context: viewContext)
+            newJob.id = UUID()
+            newJob.name = name
+            
+            do {
+                try viewContext.save()
+            } catch {
+                // Replace this implementation with code to handle the error appropriately.
+                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
+                let nsError = error as NSError
+                fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
+            }
+        }
+    }
+
+    private func deletejobs(offsets: IndexSet) {
+        withAnimation {
+            offsets.map { jobs[$0] }.forEach(viewContext.delete)
+
+            do {
+                try viewContext.save()
+            } catch {
+                // Replace this implementation with code to handle the error appropriately.
+                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
+                let nsError = error as NSError
+                fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
+            }
+        }
+    }
+}
