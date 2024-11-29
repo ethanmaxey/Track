ed9201a663b7ab7acab9fbd8d551bb4f74ce3429
ethanmaxey <ethanm113@gmail.com>
2024-11-28 20:39:37 -0700

    Setup fastlane snapshots for the most part

---
+++ Track.ioUITests/Track_ioUITests.swift
@@ -0,0 +1,62 @@
+//
+//  Track_ioUITests.swift
+//  Track.ioUITests
+//
+//  Created by Ethan Maxey on 11/28/24.
+//
+
+import XCTest
+
+final class Track_ioUITests: XCTestCase {
+    var app = XCUIApplication()
+
+    @MainActor
+    override func setUp() {
+        // UI tests must launch the application that they test.
+        app = XCUIApplication()
+        setupSnapshot(app)
+        app.launch()
+    }
+    
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testTakeMainListScreenshot() throws {
+        snapshot("MainList")
+    }
+    
+    @MainActor
+    func testTakeDetailsScreenshot() throws {
+        app.buttons["Google"].tap()
+        snapshot("Details")
+    }
+    
+    @MainActor
+    func testTakeAddJobScreenshot() throws {
+        app.buttons["Add Job"].tap()
+        
+        let textField = app.textFields["Enter job name here."]
+        textField.tap()
+        textField.typeText("NASA")
+        snapshot("AddJob")
+    }
+    
+    @MainActor
+    func testTakeSankeyScreenshot() throws {
+        app.buttons["Visualize"].tap()
+        snapshot("Sankey")
+    }
+    
+    @MainActor
+    func testTakeSankeyShareScreenshot() throws {
+        app.buttons["Visualize"].tap()
+        
+        if app.buttons["Share…"].waitForExistence(timeout: 10) {
+            app.buttons["Share…"].tap()
+        }
+        
+        snapshot("Share")
+    }
+}
---
+++ fastlane/Appfile.swift
@@ -0,0 +1,9 @@
+var appIdentifier: String { return "com.app.ethanmaxey.Track" } // The bundle identifier of your app
+var appleID: String { return "ethanm113@gmail.com" } // Your Apple Developer Portal username
+
+var itcTeam: String? { return "126755062" } // App Store Connect Team ID
+var teamID: String { return "57X39L89Y4" } // Apple Developer Portal Team ID
+
+
+// For more information about the Appfile, see:
+//     https://docs.fastlane.tools/advanced/#appfile
